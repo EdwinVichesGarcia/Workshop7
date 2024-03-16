a10a24b9247f07287e1d41758b42e6fe806cfb2d
EdwinVichesGarcia <edwinviches@gmail.com>
2024-03-16 13:18:31 -0700

    Add files via upload

---
+++ Workshop 7/javascript.js
@@ -0,0 +1,40 @@
+//Pseudocode: Create a alert displaying a message
+// Create three operation making the combination code
+//console.log them to store then in the system
+//create a second alert displaying the combination
+
+
+
+//  Using the alert to create a pop up that display the message below 
+
+alert ("You have received this message because you have been chosen to open an important vault. Here's the combination:");
+
+/* 
+Using addition to get the first numver of the combination
+Using mulitplication to get the second number of the combination   
+using subtraction to get the last number of the combination
+*/
+
+const firstNum=(5+5)
+const secondNum=(2*20)
+const thirdNum=(50-11)
+
+// Console.log the statements i made for the combination
+
+console.log (firstNum)
+console.log (secondNum)
+console.log (thirdNum)
+
+//  Using another alert to create a pop up displaying the combination
+
+//  The third number doesn't display on the alert and a error message pop ups syaing to add ',' in front of thirdNum
+
+alert (firstNum + "-" + secondNum + "-" ,thirdNum);
+
+
+
+
+
+
+
+
